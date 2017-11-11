b3b8b50c18ef7e46a2d12bd2a5621e65ec10fbe4
adamwisky <1621582+adamwisky@users.noreply.github.com>
2017-11-11 14:15:47 -0500

    optimized

--- anagramDiff.py
+++ anagramDiff.py
@@ -1,23 +1,18 @@
+from collections import defaultdict
+
 def anagram_diff(start, end):
     if len(start) == 0 or len(start) != len(end):
         raise Exception("invalid input")
-    letter_count = [0] * 26
+    letter_count = defaultdict(lambda: 0)
 
     for i in range(len(start)):
-        start_char_val = ord(start[i]) - ord('a')
-        if(start_char_val < 0 or start_char_val > 25):
-            raise Exception("invalid input")
-        letter_count[start_char_val] += 1
-
-        end_char_val = ord(end[i]) - ord('a')
-        if(end_char_val < 0 or end_char_val > 25):
-            raise Exception("invalid input")
-        letter_count[end_char_val] -= 1
+        letter_count[start[i]] += 1
+        letter_count[end[i]] -= 1
 
     return_val = 0
     for i in letter_count:
-        if i > 0:
-            return_val += i
+        if letter_count[i] > 0:
+            return_val += letter_count[i]
 
     print start, end, return_val
 
